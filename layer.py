3592e8fc4b986be68f591331ab8d1064960b5391
YuBinLab-QUST <111045765+YuBinLab-QUST@users.noreply.github.com>
2024-10-11 14:54:59 +0800

    Update layer.py

--- layer.py
+++ layer.py
@@ -1,7 +1,7 @@
 import math
 import torch
 from torch.nn.parameter import Parameter
-from torch.nn.modules.module import Module
+from torch.nn.modules.module import Module 
 
 
 class GraphConvolution(Module):
