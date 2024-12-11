dc232fddb03c412f16a0268f547ed88c25445586
YuBinLab-QUST <111045765+YuBinLab-QUST@users.noreply.github.com>
2024-12-11 19:09:30 +0800

    Update VGAE.py

--- VGAE.py
+++ VGAE.py
@@ -1,12 +1,12 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F 
+import torch.nn.functional as F
 
 from layer import GraphConvolution
 
 
 class GCNModelVAE(nn.Module):
-    def __init__(self, input_feat_dim, hidden_dim1, dropout=0):
+    def __init__(self, input_feat_dim, hidden_dim1, dropout):
         super(GCNModelVAE, self).__init__()
         self.gc1 = GraphConvolution(input_feat_dim, hidden_dim1)
         self.gc2 = GraphConvolution(hidden_dim1, hidden_dim1)
